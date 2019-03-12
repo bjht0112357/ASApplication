e2a6e4f7d484b695b2160c79c602680164507e8c
bjht0112357 <734779335@qq.com>
2019-03-12 12:00:08 +0800

    测试添加文件上传github

---
+++ app/src/main/java/org/com/asapplication/GIthubTestActivity.java
@@ -0,0 +1,13 @@
+package org.com.asapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GIthubTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_github_test);
+    }
+}
